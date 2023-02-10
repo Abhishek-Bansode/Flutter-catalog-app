21ed27c599ab7fedf26aceb5d83d488432d6755a
RyzenHydra <88792504+AbhishekPBansode@users.noreply.github.com>
2023-02-10 19:31:50 +0530

    Day 15 completed changed to GridView from ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,7 +23,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(const Duration(seconds: 2));
+    // await Future.delayed(const Duration(seconds: 2));
 
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
@@ -43,13 +42,50 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
-                  itemBuilder: (context, index) => ItemWidget(
-                    item: CatalogModel.items[index],
+          child: (CatalogModel.items.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
                   ),
-                )
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items[index];
+                    return Card(
+                        clipBehavior: Clip.antiAlias,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: GridTile(
+                            header: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: const BoxDecoration(
+                                color: Colors.deepPurple,
+                              ),
+                              child: Text(
+                                item.name,
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            footer: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: const BoxDecoration(
+                                color: Colors.black,
+                              ),
+                              child: Text(
+                                item.price.toString(),
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            child: Image.network(item.image)));
+                  },
+                  itemCount: CatalogModel.items.length)
+
+              //  ListView.builder(
+              //     itemCount: CatalogModel.items.length,
+              //     itemBuilder: (context, index) => ItemWidget(
+              //       item: CatalogModel.items[index],
+              //     ),
+              //   )
               : const Center(
                   child: CircularProgressIndicator(),
                 )),
